2eeb90135f713105cd4bfe067e3b9dba2fe26156
bueinat <62245924+bueinat@users.noreply.github.com>
2021-06-07 14:56:31 +0300

    basic app

---
+++ app/src/main/java/com/example/testapp/JoystickActivity.java
@@ -0,0 +1,10 @@
+package com.example.testapp;
+
+import android.content.Context;
+import android.view.View;
+
+public class JoystickActivity extends View {
+    public JoystickActivity(Context context) {
+        super(context);
+    }
+}
