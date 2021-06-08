565ea69230e22af6efd197d2c594e418b75c4f23
omer-zivoni <71780251+omer-zivoni@users.noreply.github.com>
2021-06-08 15:22:45 +0300

    15:22

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, JoystickActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(this, JoystickActivity.class);
+        startActivity(intent);
+    }
+
+
 }
