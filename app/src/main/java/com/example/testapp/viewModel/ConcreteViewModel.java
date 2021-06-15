6c5b4f7aa39fdb537e30fe794fc99443272912c3
omer-zivoni <71780251+omer-zivoni@users.noreply.github.com>
2021-06-16 00:32:13 +0300

    16.6

--- app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
+++ app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
@@ -23,47 +23,44 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
     public double getThrottle() {
         Log.d("throttle", "This is my message");
         return this.throttle;
-    //bind the data to ip and to port, and then call runModel
+        //bind the data to ip and to port, and then call runModel
 
-    //bind each of the properties, and then call model.set for each one of them
-    //i think the set methods behind is not necessary
-
-
-
-    public void setAileron(double new_aileron) {
-        this.throttle = new_aileron;
-        this.model.setThrottle(new_aileron);
-    }
-    public void setElevator(double new_elevator) {
-        this.throttle = new_elevator;
-        this.model.setThrottle(new_elevator);
-    }
-    public void setRudder(double new_rudder) {
-        this.throttle = new_rudder;
-        this.model.setThrottle(new_rudder);
+        //bind each of the properties, and then call model.set for each one of them
+        //i think the set methods behind is not necessary
     }
 
 
-
-    public void setThrottle(double newThrottle) {
-        Log.d("throttle", "This is my message");
-        this.throttle = newThrottle;
-        // update model's throttle
-    }
-
-    public void runModel()
-    {
-        try {
-            //ip and port for example
-            InetAddress ip = InetAddress.getByAddress(
-                    new byte[]{(byte) 212, (byte) 76, (byte) 112, (byte) 219});
-            this.model = new ConcreteModel(ip, 6400);
-            model.connectToServer();
-        } catch(Exception e)
-        {
-            System.out.println("failed to create the variable \"ip\"");
+        public void setAileron ( double new_aileron){
+            this.throttle = new_aileron;
+            this.model.setThrottle(new_aileron);
         }
-    }
+        public void setElevator ( double new_elevator){
+            this.throttle = new_elevator;
+            this.model.setThrottle(new_elevator);
+        }
+        public void setRudder ( double new_rudder){
+            this.throttle = new_rudder;
+            this.model.setThrottle(new_rudder);
+        }
 
+
+        public void setThrottle ( double newThrottle){
+            Log.d("throttle", "This is my message");
+            this.throttle = newThrottle;
+            // update model's throttle
+        }
+
+        public void runModel ()
+        {
+            try {
+                //ip and port for example
+                InetAddress ip = InetAddress.getByAddress(
+                        new byte[]{(byte) 212, (byte) 76, (byte) 112, (byte) 219});
+                this.model = new ConcreteModel(ip, 6400);
+                model.connectToServer();
+            } catch (Exception e) {
+                System.out.println("failed to create the variable \"ip\"");
+            }
+        }
 
 }
