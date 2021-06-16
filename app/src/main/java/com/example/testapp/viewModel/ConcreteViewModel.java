e3df51929f104bbf50638ac7c2fe70c06c180214
bueinat <62245924+bueinat@users.noreply.github.com>
2021-06-16 15:56:03 +0300

    merge + minor changes

--- app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
+++ app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
@@ -87,43 +87,29 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
         notifyPropertyChanged(BR.throttle);
         // update model's throttle
     }
-        public void setThrottle ( double newThrottle){
-            Log.d("throttle", "This is my message");
-            this.throttle = newThrottle;
-            // update model's throttle
-        }
 
+                private double progressToValue(int progress) {
+                return (double)(progress - 100) / 100;
+            }
     @Bindable
     public int getThrottleProgress() {
-        Log.d("ThrottleProgress", "This is my message");
+        Log.d("ThrottleProgress get", "This is my message " + progressToValue((this.throttleProgress)));
         return this.throttleProgress;
     }
 
     @Bindable
     public void setThrottleProgress(int newThrottle) {
-        Log.d("ThrottleProgress", "This is my message");
+        Log.d("ThrottleProgress set", "This is my message " + progressToValue((this.throttleProgress)));
         this.throttleProgress = newThrottle;
         notifyPropertyChanged(BR.throttleProgress);
         // update model's throttle
     }
 
     @Override
-    public void runModel()
-    {
-        try {
-            InetAddress ip = InetAddress.getByAddress(
-                    new byte[]{(byte) 212, (byte) 76, (byte) 112, (byte) 219});
-            ConcreteModel model = new ConcreteModel(ip, 6400);
-            model.connectToServer();
-        } catch(Exception e)
-        {
-            System.out.println("failed to create the variable \"ip\"");
-        }
-    }
         public void runModel ()
         {
             try {
-                //ip and port for example
+                // ip and port for example
                 InetAddress ip = InetAddress.getByAddress(
                         new byte[]{(byte) 212, (byte) 76, (byte) 112, (byte) 219});
                 this.model = new ConcreteModel(ip, 6400);
