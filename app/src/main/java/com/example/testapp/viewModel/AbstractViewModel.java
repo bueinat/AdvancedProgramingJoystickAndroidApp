3e0e8ac25711fd30a5c13f17035eccbcbef728eb
bueinat <62245924+bueinat@users.noreply.github.com>
2021-06-17 10:27:47 +0300

    update viewModel

--- app/src/main/java/com/example/testapp/viewModel/AbstractViewModel.java
+++ app/src/main/java/com/example/testapp/viewModel/AbstractViewModel.java
@@ -3,9 +3,23 @@ package com.example.testapp.viewModel;
 import java.io.Serializable;
 
 public interface AbstractViewModel extends Serializable {
-//    public double throttle;
-//    public void setThrottle(double new_throttle) { this.throttle = new_throttle; }
+
+    double getThrottle();
     void setThrottle(double new_throttle);
+    int getThrottleProgress();
+    void setThrottleProgress(int new_throttle_progress);
+
+    double getRudder();
+    void setRudder(double new_rudder);
+    int getRudderProgress();
+    void setRudderProgress(int new_rudder_progress);
+
+    double getAileron();
+    void setAileron(double new_aileron);
+
+    double getElevator();
+    void setElevator(double new_elevator);
+
     void runModel();
 
 }
--- app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
+++ app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
@@ -12,56 +12,43 @@ import java.net.InetAddress;
 
 public class ConcreteViewModel extends BaseObservable implements AbstractViewModel {
 
+    // bind the data to ip and to port, and then call runModel
+    // bind each of the properties, and then call model.set for each one of them
+    // I think the set methods behind is not necessary
 
     private ConcreteModel model;
     private InetAddress ip;
     private int port;
 
-    private double aileron;
-    private double elevator;
-    private double rudder;
+    private double progressToValue(int progress) { return (double)(progress - 100) / 100; }
+    private int valueToProgress(double value) { return (int)((value + 1) * 100); }
+
     @Bindable
-    private double throttle;
+    private double aileron;
+    @Bindable
+    private double elevator;
+    @Bindable
+    private double rudder;
 
     @Bindable
     private int throttleProgress = 100;
 
-
-    // bind the data to ip and to port, and then call runModel
-    // bind each of the properties, and then call model.set for each one of them
-    // i think the set methods behind is not necessary
-
+    @Override
     @Bindable
     public double getThrottle() {
         Log.d("throttle", "This is my message");
-        return this.throttle;
+        return progressToValue(this.throttleProgress);
     }
-
-
-        public void setAileron(double new_aileron) {
-            this.throttle = new_aileron;
-            this.model.setAileron(new_aileron);
-        }
-        public void setElevator(double new_elevator) {
-            this.throttle = new_elevator;
-            this.model.setElevator(new_elevator);
-        }
-        public void setRudder(double new_rudder) {
-            this.throttle = new_rudder;
-            this.model.setRudder(new_rudder);
-        }
-
 
     @Override
     @Bindable
-    public void setThrottle(double newThrottle) {
-        Log.d("throttle", "This is my message");
-        this.throttle = newThrottle;
-        notifyPropertyChanged(BR.throttle);
+    public void setThrottle(double new_throttle) {
         // update model's throttle
+        Log.d("throttle", "This is my message");
+        setThrottleProgress(valueToProgress(new_throttle));
+        notifyPropertyChanged(BR.throttle);
     }
 
-    private double progressToValue(int progress) { return (double)(progress - 100) / 100; }
     @Bindable
     public int getThrottleProgress() {
         Log.d("ThrottleProgress get", "This is my message " + progressToValue((this.throttleProgress)));
@@ -69,26 +56,83 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
     }
 
     @Bindable
-    public void setThrottleProgress(int newThrottle) {
+    public void setThrottleProgress(int new_throttle) {
         Log.d("ThrottleProgress set", "This is my message " + progressToValue((this.throttleProgress)));
-        this.throttleProgress = newThrottle;
+        this.throttleProgress = new_throttle;
         notifyPropertyChanged(BR.throttleProgress);
-        // update model's throttle
+        this.model.setThrottle(getRudder());
+    }
+
+    @Bindable
+    private int rudderProgress = 100;
+
+    @Override
+    @Bindable
+    public double getRudder() {
+        Log.d("rudder", "This is my message");
+        return progressToValue(this.rudderProgress);
     }
 
     @Override
-        public void runModel ()
-        {
-            Log.d("runModel", "This is my message");
-            try {
-                // ip and port for example
-                InetAddress ip = InetAddress.getByAddress(
-                        new byte[]{(byte) 212, (byte) 76, (byte) 112, (byte) 219});
-                this.model = new ConcreteModel(ip, 6400);
-                model.connectToServer();
-            } catch (Exception e) {
-                System.out.println("failed to create the variable \"ip\"");
-            }
+    @Bindable
+    public void setRudder(double new_rudder) {
+        // update model's throttle
+        Log.d("rudder", "This is my message");
+        setRudderProgress(valueToProgress(new_rudder));
+        notifyPropertyChanged(BR.rudder);
+    }
+
+    @Bindable
+    public int getRudderProgress() {
+        Log.d("RudderProgress get", "This is my message " + progressToValue((this.rudderProgress)));
+        return this.rudderProgress;
+    }
+
+    @Bindable
+    public void setRudderProgress(int new_rudder) {
+        Log.d("ThrottleProgress set", "This is my message " + progressToValue((this.rudderProgress)));
+        this.rudderProgress = new_rudder;
+//        notifyPropertyChanged(BR.rudderProgress);
+        this.model.setRudder(getRudder());
+    }
+
+
+    public void setAileron(double new_aileron) {
+        this.aileron = new_aileron;
+        this.model.setAileron(new_aileron);
+    }
+
+    @Override
+    public double getElevator() {
+        return this.elevator;
+    }
+
+    public void setElevator(double new_elevator) {
+        this.elevator = new_elevator;
+        this.model.setElevator(new_elevator);
+    }
+
+
+    @Override
+    public double getAileron() {
+        return this.aileron;
+    }
+
+    @Override
+    public void runModel ()
+    {
+        // what happens if you click the button after
+        // you've already connected
+        Log.d("runModel", "This is my message");
+        try {
+            // ip and port for example
+            InetAddress ip = InetAddress.getByAddress(
+                    new byte[]{(byte) 212, (byte) 76, (byte) 112, (byte) 219});
+            this.model = new ConcreteModel(ip, 6400);
+            model.connectToServer();
+        } catch (Exception e) {
+            System.out.println("failed to create the variable \"ip\"");
         }
+    }
 
 }
