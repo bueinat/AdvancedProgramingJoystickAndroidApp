029e7f7a2e922574dbc22f215bb5b62207f83f13
bueinat <62245924+bueinat@users.noreply.github.com>
2021-06-24 11:08:11 +0300

    throttle fixed

--- app/src/main/java/com/example/testapp/view/JoystickActivity.java
+++ app/src/main/java/com/example/testapp/view/JoystickActivity.java
@@ -22,23 +22,17 @@ public class JoystickActivity extends AppCompatActivity {
     private ConcreteViewModel VM;
     private Joystick joystick;
 
-    @BindingAdapter({"toastMessage"})
-    public static void runMe(View view, String message) {
-        Log.d("myTag", "runme message");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         // data binding
         ActivityJoystickBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_joystick);
         VM = new ConcreteViewModel();
-        VM.runModel();
         binding.setViewModel(VM);
         binding.executePendingBindings();
 
+        // get joystick and inject operations to joystick
         joystick = (Joystick) findViewById(R.id.joystickView);
 
         joystick.service=(a,e)->{
--- app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
+++ app/src/main/java/com/example/testapp/viewModel/ConcreteViewModel.java
@@ -44,8 +44,10 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
     }
 
 
-    private double progressToValue(int progress) { return (double)(progress - 100) / 100; }
-    private int valueToProgress(double value) { return (int)((value + 1) * 100); }
+    private double progressToRudder(int progress) { return (double)(progress - 100) / 100; }
+    private int rudderToProgress(double value) { return (int)((value + 1) * 100); }
+    private double progressToThrottle(int progress) { return (double)(progress - 50) / 50; }
+    private int throttleToProgress(double value) { return (int)((value + 1) * 50); }
 
     @Bindable
     private double aileron;
@@ -59,7 +61,7 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
     @Bindable
     public double getThrottle() {
         Log.d("throttle", "This is my message");
-        return progressToValue(this.throttleProgress);
+        return progressToThrottle(this.throttleProgress);
         //bind the data to ip and to port, and then call runModel
 
         //bind each of the properties, and then call model.set for each one of them
@@ -72,13 +74,13 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
     public void setThrottle(double new_throttle) {
         // update model's throttle
         Log.d("throttle", "This is my message");
-        setThrottleProgress(valueToProgress(new_throttle));
+        setThrottleProgress(throttleToProgress(new_throttle));
         notifyPropertyChanged(BR.throttle);
     }
 
     @Bindable
     public int getThrottleProgress() {
-        Log.d("ThrottleProgress get", "This is my message " + progressToValue(this.throttleProgress));
+        Log.d("ThrottleProgress get", "This is my message " + progressToThrottle(this.throttleProgress));
         return this.throttleProgress;
     }
 
@@ -95,14 +97,14 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
 
     @Override
     @Bindable
-    public double getRudder() { return progressToValue(this.rudderProgress); }
+    public double getRudder() { return progressToRudder(this.rudderProgress); }
 
     @Override
     @Bindable
     public void setRudder(double new_rudder) {
         // update model's throttle
         Log.d("rudder", "This is my message");
-        setRudderProgress(valueToProgress(new_rudder));
+        setRudderProgress(rudderToProgress(new_rudder));
         notifyPropertyChanged(BR.rudder);
     }
 
@@ -111,7 +113,7 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
 
     @Bindable
     public void setRudderProgress(int new_rudder) {
-//        Log.d("ThrottleProgress set", "This is my message " + progressToValue(this.rudderProgress));
+        Log.d("ThrottleProgress set", "This is my message " + progressToRudder(this.rudderProgress));
         this.rudderProgress = new_rudder;
 //        notifyPropertyChanged(BR.rudderProgress);
         this.model.setRudder(getRudder());
@@ -146,8 +148,7 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
     @Override
     public void runModel ()
     {
-        // what happens if you click the button after
-        // you've already connected
+        // what happens if you click the button after you've already connected
 //        Log.d("runModel", "This is my message");
         try {
 
@@ -157,10 +158,11 @@ public class ConcreteViewModel extends BaseObservable implements AbstractViewMod
             this.model = new ConcreteModel(ip, 5400);
 
 //            this.model = new ConcreteModel(this.ip, this.port);
-            this.model.setThrottle(1.0);
-            this.model.setRudder(0.1);
-            this.model.setAileron(0.2);
-            model.connectToServer();
+            this.model.setThrottle(0.0);
+            this.model.setRudder(0.0);
+            this.model.setAileron(0.0);
+            this.model.setElevator(0.0);
+            this.model.connectToServer();
         } catch (Exception e) {
             System.out.println("failed to create the variable \"ip\"");
         }
